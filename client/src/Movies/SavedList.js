7bc9453ccd5b537f51edcebcc3756b0a2ecf1c77
Seth Nadu <seth.nadu@gmail.com>
2019-07-22 18:33:44 -0400

    navlink stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import MovieList from "./MovieList";
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <Link to ={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></Link>
+      <NavLink  to ={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>
     ))}
     <Link to="/"><div className="home-button">Home</div></Link>
   </div>
